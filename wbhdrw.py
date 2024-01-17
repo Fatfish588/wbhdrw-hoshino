ebaf3423bda04b77d442a81b78da502d01b561bf
Fatfish588 <59791439+Fatfish588@users.noreply.github.com>
2024-01-17 10:16:51 +0800

    更新 wbhdrw.py

--- wbhdrw.py
+++ wbhdrw.py
@@ -1,8 +1,13 @@
+import re
+
 import easyocr
 
 from hoshino import Service
 from hoshino.typing import CQEvent
-
+from hoshino.config import NICKNAME
+from skimage import io
+if type(NICKNAME) == str:
+    NICKNAME = [NICKNAME]
 sv = Service(
     name="我不会打日文啊",  # 功能名
     visible=True,  # 可见性
@@ -12,13 +17,53 @@ sv = Service(
 
 )
 
+# gocq和山姆摇滚的图片上报稍有不同。已测试这么写都能适配。
+regex_image = re.compile(
+    r'\[CQ:image,file=[a-fA-F0-9]{32}.*?,url=(https?://gchat.qpic.cn/gchatpic_new/\d+/\d+-\d+-[a-fA-F0-9]{32}/0\?term=\d.*?)\]')
+
+
+def create_all_messages_data(msg_list, ev: CQEvent):
+    messages_data_list = [{
+        "type": "node",
+        "data": {
+            "name": str(NICKNAME[0]),
+            "user_id": str(ev.self_id),
+            "content": "以下为识别到的内容。因为是纯日文识别，所以请忽略其他内容，找到想要的日文部分即可。"
+        }
+    }]
+    for msg in msg_list:
+        data = {
+            "type": "node",
+            "data": {
+                "name": str(NICKNAME[0]),
+                "user_id": str(ev.self_id),
+                "content": str(msg[1])
+            }
+        }
+        messages_data_list.append(data)
+
+    messages_data_list.append({
+        "type": "node",
+        "data": {
+            "name": str(NICKNAME[0]),
+            "user_id": str(ev.self_id),
+            "content": "以上为识别到的内容。因为是纯日文识别，所以请忽略其他内容，找到想要的日文部分即可。"
+        }
+    })
+    return messages_data_list
+
+
 @sv.on_keyword("我不会打日文")
-async def I_dont_know_how_to_type_Japanese(bot, ev: CQEvent):
-    # await bot.send(ev, result, at_sender=False)
-    await bot.send_group_forward_msg(group_id=ev["group_id"], messages=["第一行", "第二行"])
-
-
-if __name__ == '__main__':
-    reader = easyocr.Reader(['ja'])
-    result = reader.readtext('test.jpg')
-    print(result)
+async def I_dont_know_how_to_type_japanese(bot, ev: CQEvent):
+    images = regex_image.findall(ev.raw_message)
+    if len(images) < 1:
+        bot.send(ev, "我不会打日文：没有收到图片呢，请和文字一起发送", at_sender=False)
+    elif len(images) > 1:
+        bot.send(ev, "我不会打日文：目前只能接受一张图呢", at_sender=False)
+    else:
+        image = io.imread(images[0])
+        bot.send(ev, "正在识别图中日文，请稍后", at_sender=False)
+        reader = easyocr.Reader(['ja'])
+        result = reader.readtext(image)
+        send_messages_list = create_all_messages_data(result, ev)
+        await bot.send_group_forward_msg(group_id=ev["group_id"], messages=send_messages_list)
